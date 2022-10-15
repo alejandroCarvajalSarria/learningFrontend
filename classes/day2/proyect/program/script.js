34beeefbe764948fe577f0b33f552bfed5a10b00
Alejandro Carvajal <alejandro.carvajal@yuxiglobal.com>
2022-10-15 16:54:33 -0500

    Indentation Fix

--- classes/day2/proyect/program/script.js
+++ classes/day2/proyect/program/script.js
@@ -1,7 +1,8 @@
-let titulo1 = document.getElementById("titulo");
-let counter = 0
-function changeText() {
-titulo1.innerText = `Cacahuetes ${counter}`;
-counter++;
-}
-titulo1.addEventListener("mouseover", changeText);
+
+    let titulo1 = document.getElementById("titulo");
+    let counter = 0
+    function changeText() {
+        titulo1.innerText = `Cacahuetes ${counter}`;
+        counter++;
+    }
+    titulo1.addEventListener("mouseover", changeText);
